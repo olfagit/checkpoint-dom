58f96d5209dfbd1c27459ab0410b7d4687356f22
Mohamed Sadok Yahyaoui <mohamedsadokyahyaoui@gmail.com>
2022-02-28 09:49:24 +0100

    adding function sum

--- script.js
+++ script.js
@@ -1,27 +1,31 @@
-var price = document.querySelector(".float-right text-right");
-var plus = document.querySelectorAll(".fas fa-plus");
-var minus = document.querySelectorAll(".fas fa-minus");
-var quantity = document.querySelectorAll("quantity");
+var price = document.querySelector(".total");
+var plus = document.querySelectorAll(".fa-plus");
+var minus = document.querySelectorAll(".fa-minus");
+var quantity = document.querySelectorAll(".quantity");
 var unitprice = document.querySelectorAll(".unit-price");
-var heart = document.querySelectorAll(".fas fa-heart");
+var heart = document.querySelectorAll(".fa-heart");
 
 
+function Subtotal() {
+    let sum = 0;
+    for (i = 0; i < quantity.length; i++) {
+        sum = sum + Number(quantity[i].innerText) * Number(unitprice[i].innerText);
+    }
+    price.innerText = sum
+}
 
 for (let i = 0; i < plus.length; i++) {
     plus[i].addEventListener("click", function() {
-        quantity[i].innerTEXT = Number(quantity[i].innerTEXT) + 1;
+        quantity[i].innerText = Number(quantity[i].innerText) + 1;
+        Subtotal()
     });
 }
 for (let i = 0; i < minus.length; i++) {
     minus[i].addEventListener("click", function() {
-        quantity[i].innerTEXT = Number(quantity[i].innerTEXT) - 1;
+        if (quantity[i].innerText > 0) {
+            quantity[i].innerText = Number(quantity[i].innerText) - 1
+            Subtotal()
+        };
     });
 
-}
-
-function Subtotal() {
-    sum = 0;
-    for (i = 0; i < quantity.length; i++) {
-        sum = Number(quantity[i].innerTEXT) * Number(unitprice[i].innerTEXT);
-    }
 }
